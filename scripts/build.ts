8109a577a7e5bcbfe2c5c624d8673cce296ac96a
litingyes <luz.liting@gmail.com>
2025-01-26 17:54:39 +0800

    fix(build): build order for components

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,6 +3,12 @@ import { readFileSync } from 'node:fs'
 import { globSync } from 'fast-glob'
 
 function main() {
+  /**
+   * some components should build after someone
+   * e.g. button depends on icon
+   */
+  const componentsOrder = ['icon', 'button']
+
   const toBuilds = [
     '@ohvue/utils',
     '@ohvue/theme',
@@ -11,6 +17,11 @@ function main() {
       ignore: ['**/node_modules/**'],
     }).map((path) => {
       return JSON.parse(readFileSync(path, 'utf-8')).name
+    }).sort((a: string, b: string) => {
+      const indexOfA = componentsOrder.findIndex(item => a.includes(item))
+      const indexOfB = componentsOrder.findIndex(item => b.includes(item))
+
+      return indexOfA - indexOfB
     }),
     '@ohvue/storybook',
     'ohvue',
